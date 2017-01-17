4661c739dfa77b08378aaf0c6e96cbe6c7af2945
Vinicius Celms <by.vinicius@gmail.com>
2017-01-17 16:30:58 -0200

    Returning the number of repetitions of the word

--- core/scraping.py
+++ core/scraping.py
@@ -1,7 +1,22 @@
 import urllib.request
 from lxml import html
+from bs4 import BeautifulSoup
 
 class Scraping():
 
-    def count_word(url, selected_word)::
-        pass
+    def count_word(url, selected_word):
+        html = urllib.request.urlopen(str(url)).read().decode("utf-8")
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        content = soup.get_text()
+
+        words = content.split()
+
+        word_quantity = 0
+
+        for word in words:
+            if word == selected_word:
+                word_quantity += 1
+
+        return word_quantity
