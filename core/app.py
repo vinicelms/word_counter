7e69fa93c17a5df73215b084b6caba82da56d9c8
Vinicius Celms <by.vinicius@gmail.com>
2017-01-17 17:21:57 -0200

    Getting data from URL parameters

--- core/app.py
+++ core/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from scraping import Scraping
 import json
 
@@ -8,9 +8,17 @@ api = Api(app)
 
 class Word_Counter(Resource):
     def get(self):
-        quantity = Scraping.count_word("https://docs.python.org/3.5/reference/index.html", "Python")
+        parser = reqparse.RequestParser()
+        parser.add_argument('url', type=str)
+        parser.add_argument('word', type=str)
+        args = parser.parse_args()
+
+        url = args.get('url')
+        word = args.get('word')
+
+        quantity = Scraping.count_word(url, word)
         word_counted = {}
-        word_counted[str("Python")] = quantity
+        word_counted[str(word)] = quantity
         return jsonify(word_counted)
 
 api.add_resource(Word_Counter, '/word_counter')
