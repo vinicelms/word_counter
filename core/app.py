6caa918cde819a9af5d7a3b33035ea312b007b56
Vinicius Celms <by.vinicius@gmail.com>
2017-01-17 17:11:55 -0200

    Correct return of a Json with the word count

--- core/app.py
+++ core/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from scraping import Scraping
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 class Word_Counter(Resource):
     def get(self):
-        return {"status": "Construction"}
+        quantity = Scraping.count_word("https://docs.python.org/3.5/reference/index.html", "Python")
+        word_counted = {}
+        word_counted[str("Python")] = quantity
+        return jsonify(word_counted)
 
 api.add_resource(Word_Counter, '/word_counter')
 
